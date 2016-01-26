7c8a219f6f4a298af7a469247f6179e91b4e79e3
Paul Radzkov <radzkov@gmail.com>
2016-01-26 14:59:44 +0300

    mobile nav scripts

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -186,4 +186,17 @@
         });
     }
 
+    //mobile nav begin
+    $('#mobileNav').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var page = button.data('menupage'); // Extract info from data-* attributes
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('.mobilenav-page').hide();
+        modal.find(''+page+'').show();
+
+        console.info(button, page, modal);
+    });
+    //mobile nav end
+
 }(jQuery));
